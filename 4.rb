f06852c1952a9166ff31a9decf4b78e298270c81
Lena <lena4ka94@inbox.ru>
2018-10-01 21:49:36 +0300

    Внесла правки по замечаниям

--- 4.rb
+++ 4.rb
@@ -1,13 +1,11 @@
 #  Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1)
 
 letters = {}
-vowels = ["a", "e", "i", "o", "u", "y"]
+vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 index = 1
 
-for letter in ("a".."z")
-  if vowels.include?(letter)
-    letters[letter] = index
-  end
+for letter in ("a".."z") 
+  letters[letter] = index if vowels.include?(letter)
   index += 1
 end
 
